4127ff02de0e6b9d150816684c27ff210474d3cb
BT_2N2_12 <sample@bths.edu>
2023-09-22 14:02:17 -0400

    3

--- src/CatRunner.java
+++ src/CatRunner.java
@@ -1,5 +1,11 @@
 public class CatRunner {
     public static void main(String[] args) {
         Cat cat1 = new Cat("Bubbles", 2, 3.5);
+        cat1.introduce();
+        cat1.printCatInfo();
+
+        Cat cat2 = new Cat("2", 3, 4.5);
+        cat1.introduce();
+        cat1.printCatInfo();
     }
 }
